89ea4a06333d9dacf0f66570e37303bfe1b5890c
marharytaskubak <skubak1604@gmail.com>
2023-02-04 14:50:01 +0200

    lesson3

--- lesson2/script.js
+++ lesson2/script.js
@@ -1,9 +1,9 @@
-let userName = prompt("What`s your name?")
+let name = prompt("What`s your name?")
 
-let userSurname = prompt("What`s your surname?")
+let surname = prompt("What`s your surname?")
 
-let userBirth = prompt("When did you birth?")
+let birth = prompt("When did you birth?")
 
-let userAge = 2023 - userBirth + " years old"
+let age = 2023 - birth + " years old"
 
-console.log("User Bio:", userName, userSurname,",", userAge)
+console.log(`User Bio: ${name}, ${surname}, ${age}`)
